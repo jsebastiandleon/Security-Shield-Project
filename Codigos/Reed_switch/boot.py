6ae37a9c02a3e8eda2211abea47ddbe501af26c0
cacostad <30605073+cacostad@users.noreply.github.com>
2022-07-06 22:25:14 -0500

    Add files via upload

---
+++ Codigos/Reed_switch/boot.py
@@ -0,0 +1,21 @@
+
+import network
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+def do_connect(SSID='Redmi 9',password='0e6aa3950274'):
+    global wlan
+    wlan= network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('conectando a: ', SSID,'...' )
+        wlan.connect(SSID,password)
+        while not wlan.isconnected():
+            pass
+    print('configuracion de red:', wlan.ifconfig())
+
+do_connect()
---
+++ Codigos/Reed_switch/main.py
@@ -0,0 +1,50 @@
+#REMEMBER TO RENAME FILE TO main.py BEFORE UPLOAING TO THE BOARD
+
+from utelegram import Bot
+from utime import sleep
+from machine import Pin
+
+#Configuracion de Telegram
+TOKEN = '5596407792:AAFey_P4G1gq7b4excdBUiCXGgILoHW449Q'
+bot = Bot(TOKEN)
+id=2005590886
+
+#Configuracion Reed switch
+reed = Pin(19, Pin.IN, Pin.PULL_UP)
+   
+        
+#Comandos de telegram
+@bot.add_command_handler('help')
+def help(update):
+    update.reply('Escibe gas para ver las lecturas de metano y humo \nEscribe cámara para tomar una foto y visualizarla \nEscribe puerta para ver el estado de la puerta \nEscribe 123 para llamar a emergencias')
+
+
+@bot.add_message_handler('^puerta|Puerta|PUERTA$')
+def puerta(update):
+    reed_lecture()
+
+#Funciones del sensor reed
+
+def reed_lecture():
+    if reed.value():
+        bot.send_message(chat_id=id, text='La puerta esta abierta')
+    else:
+        bot.send_message(chat_id=id, text='La puerta esta cerrada')
+
+def reed_alert():
+    if reed.value():
+        bot.send_message(chat_id=id, text='La puerta esta abierta')
+
+        
+def main ():
+    while True:
+        reed_alert()
+        sleep(5)
+
+
+        
+
+bot.start_loop(main, ())
+
+
+
